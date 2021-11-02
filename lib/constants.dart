78698a3ba00c18b7371548cfa91cd2eb6898b9f1
Arthur <ratsocial@gmail.com>
2021-11-02 10:56:17 +0800

    1102

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//testing 
 
 const textFieldDecoration = InputDecoration(
   fillColor: Colors.white,
--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_tutorial/modals.dart';
+import 'package:firebase_tutorial/services/database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_tutorial/constants.dart';
+import 'package:provider/provider.dart';
 
 class SettingsForm extends StatefulWidget {
   const SettingsForm({Key? key}) : super(key: key);
@@ -12,85 +16,118 @@ class _SettingsFormState extends State<SettingsForm> {
   final _formKey = GlobalKey<FormState>();
   final List<String> sugars = ['0', '1', '2', '3', '4', '5'];
 
-  late String currentName;
-  late String currentEmail;
-   String? currentSugar;
-
-   void setsugar(String value){
-     setState(() {
-       currentSugar = value;
-     });
-   }
+   String? currentName;
+   String? currentEmail;
+  String? currentSugar;
+  int currentStrength = 100;
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Text(
-            'Update Settings',
-            style: TextStyle(fontSize: 20),
-          ),
-          SizedBox(height: 30),
-          TextFormField(
-            decoration: textFieldDecoration,
-            validator: (val) => val!.isEmpty ? 'Enter an Name' : null,
-            // obscureText: true,
-            onChanged: (val) {
-              setState(() {
-                currentName = val;
-              });
-            },
-          ),
-          SizedBox(height: 30),
-          TextFormField(
-            decoration: textFieldDecoration,
-            validator: (val) => val!.isEmpty ? 'Enter an Email' : null,
-            // obscureText: true,
-            onChanged: (val) {
-              setState(() {
-                currentEmail = val;
-              });
-            },
-          ),
-          SizedBox(height: 30),
-          DropdownButtonFormField(
-            decoration: textFieldDecoration,
-            value: currentSugar ?? '0',
-            onChanged: (String? newValue) {
-              setState(() {
-                currentSugar = newValue!;
-              });
-            },
-            items: sugars.map((e) {
-              return DropdownMenuItem(
-                value: e,
-                child: Text('$e sugars'),
-              );
-            }).toList(),
-          ),
-          SizedBox(height: 30),
+    final user = Provider.of<User?>(context);
 
+    return StreamBuilder<UserModal>(
+        stream: DatabaseService(user!.uid).docSnap,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserModal? userData = snapshot.data;
+            return Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text(
+                      'Update Settings',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(height: 30),
+                    TextFormField(
+                      initialValue: userData?.name,
+                      decoration: textFieldDecoration,
+                      validator: (val) => val!.isEmpty ? 'Enter an Name' : null,
+                      // obscureText: true,
+                      onChanged: (val) {
+                        setState(() {
+                          currentName = val;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 30),
+                    TextFormField(
+                      initialValue: userData?.email,
+                      decoration: textFieldDecoration,
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter an Email' : null,
+                      // obscureText: true,
+                      onChanged: (val) {
+                        setState(() {
+                          currentEmail = val;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 30),
+                    DropdownButtonFormField(
+                      decoration: textFieldDecoration,
+                      value: currentSugar ?? '0',
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          currentSugar = newValue!;
+                        });
+                      },
+                      items: sugars.map((e) {
+                        return DropdownMenuItem(
+                          value: e,
+                          child: Text('$e sugars'),
+                        );
+                      }).toList(),
+                    ),
+                    SizedBox(height: 30),
+                    Slider(
+                      activeColor: Colors.pink[currentStrength],
+                      inactiveColor: Colors.grey,
+                      min: 100,
+                      max: 900,
+                      divisions: 8,
+                      label: currentStrength.toString(),
+                      onChanged: (val) {
+                        setState(() {
+                          currentStrength = val.round();
+                        });
+                      },
+                      value: (currentStrength).toDouble(),
+                    ),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                        child: Text('Update'),
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            //   await Future.delayed(Duration(seconds: 3));
+                            if (currentSugar == null) {
+                              currentSugar = '0';
+                            }
 
+                            if (currentStrength == null) {
+                              currentStrength = 100;
+                            }
 
-          
-          SizedBox(height: 30),
-          ElevatedButton(
-              child: Text('Update'),
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  //   await Future.delayed(Duration(seconds: 3));
-                  if(currentSugar == null){
-                    currentSugar = '0';
-                  }
-                  print(currentName);
-                  print(currentEmail);
-                  print(currentSugar);
-                }
-              }),
-        ],
-      ),
-    );
+                            currentName ??= userData!.name;
+
+                            currentEmail ??= userData!.email;
+                            
+
+                            await result = DatabaseService(userData!.uid).updateUserData(currentName!,currentEmail!);
+                            print(currentName);
+                            print(currentEmail);
+                            print(currentSugar);
+                            print(currentStrength);
+                          }
+                        }),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return Container();
+          }
+        });
   }
 }
